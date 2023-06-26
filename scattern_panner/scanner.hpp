ee21c786f5cafb7a660d77209ca02103b0b27495
Verba de Quibus <static.faded.cpp@gmail.com>
2023-06-26 19:04:17 -0400

    did some stuff with configs, just thinking where this should go

--- scattern_panner/scanner.hpp
+++ scattern_panner/scanner.hpp
@@ -1,4 +1,24 @@
 #pragma once
 
-// TODO: step 1. create scattern panner class
-// TODO: step 2. make it find process 
+#include <functional>
+#include <Windows.h>
+
+namespace scanner_cfg_templates
+{
+	const auto page_flag_check_default = [](const std::uintptr_t page_flags) -> bool
+	{
+		if (page_flags != PAGE_GUARD and page_flags != PAGE_NOACCESS and (page_flags == PAGE_READWRITE or page_flags == PAGE_EXECUTE or page_flags == PAGE_EXECUTE_READWRITE or page_flags == PAGE_EXECUTE_READ or page_flags == PAGE_READONLY))
+			return true;
+		return false;
+	};
+}
+
+// Struct for configuring scans.
+struct scan_cfg
+{
+	// This is the page flag check, so if you want to filter it by access flags you can do so.
+	std::function<bool(const std::uintptr_t)> page_flag_check = scanner_cfg_templates::page_flag_check_default;
+
+	// Minimum and Maximum page size (in bytes)
+	std::uintptr_t min_page_size, max_page_size = 0ull, 0ull;
+};

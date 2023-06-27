02d67c0a9d4897c1743b6c79c10938093025cf38
Verba de Quibus <static.faded.cpp@gmail.com>
2023-06-27 11:54:40 -0400

    push changes

--- scattern_panner/scanner.cpp
+++ scattern_panner/scanner.cpp
@@ -1,1 +1,51 @@
 #include "scanner.hpp"
+
+process::process(const std::string_view process_name)
+{
+	std::vector<std::uint32_t> pid_list(1024); // adjust these values if you're running into index errors
+	std::vector<HMODULE> module_list(512);
+	DWORD n_pids{ 0 };
+	K32EnumProcesses(reinterpret_cast<DWORD*>(pid_list.data()), static_cast<std::uint32_t>(pid_list.capacity()) * sizeof(DWORD), &n_pids);
+
+	pid_list.resize(n_pids / 4); // shrink that heap alloc so it isn't that bad
+	std::string module_name(260, '\x0'); // MAX_FILE
+	const auto default_mod_name = module_name;
+
+	for (const auto i : pid_list)
+	{
+		if (const auto proc_handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, i); proc_handle)
+		{
+			DWORD n_modules{ 0 };
+			if (K32EnumProcessModulesEx(proc_handle, module_list.data(), static_cast<std::uint32_t>(module_list.capacity()) * sizeof(HMODULE), &n_modules, LIST_MODULES_ALL))
+			{
+				module_list.resize(n_modules / sizeof(HMODULE));
+				for (const auto& j : module_list)
+				{
+					if (K32GetModuleBaseNameA(proc_handle, j, module_name.data(), 260))
+					{
+						// Reason for this is that we're going to just have zeroes
+						std::erase_if(module_name, [](const char c)
+							{
+								if (c == 0)
+									return true;
+								return false;
+							});
+
+						if (module_name == process_name.data())
+						{
+							this->curr_proc = proc_handle;
+							this->pid = i;
+							return;
+						}
+					}
+
+					// Horribly inefficient but .clear will nuke everything so what choice do I have.
+					module_name = default_mod_name;
+				}
+
+				module_list.clear();
+				module_list.resize(1024);
+			}
+		}
+	}
+}
--- scattern_panner/scanner.hpp
+++ scattern_panner/scanner.hpp
@@ -1,17 +1,27 @@
 #pragma once
 
 #include <functional>
+#include <iostream>
 #include <Windows.h>
+#include <string_view>
+#include <Psapi.h>
+#include <processthreadsapi.h>
+#include <thread>
 
 namespace scanner_cfg_templates
 {
 	const auto page_flag_check_default = [](const std::uintptr_t page_flags) -> bool
 	{
-		if (page_flags != PAGE_GUARD and page_flags != PAGE_NOACCESS and (page_flags == PAGE_READWRITE or page_flags == PAGE_EXECUTE or page_flags == PAGE_EXECUTE_READWRITE or page_flags == PAGE_EXECUTE_READ or page_flags == PAGE_READONLY))
+		if (page_flags != PAGE_NOACCESS and page_flags != PAGE_EXECUTE and page_flags != PAGE_GUARD)
 			return true;
 		return false;
 	};
 }
+
+struct scan_result
+{
+	std::uintptr_t loc;
+};
 
 // Struct for configuring scans.
 struct scan_cfg
@@ -20,5 +30,23 @@ struct scan_cfg
 	std::function<bool(const std::uintptr_t)> page_flag_check = scanner_cfg_templates::page_flag_check_default;
 
 	// Minimum and Maximum page size (in bytes)
-	std::uintptr_t min_page_size, max_page_size = 0ull, 0ull;
+	std::uintptr_t min_page_size = 0ull, max_page_size = ~0ull;
+
+	std::function<std::vector<scan_result>()> scan_routine; // Ignore this shit atm
 };
+
+// Struct for process, this will just have like the pid and the PROCESS
+struct process
+{
+	std::uintptr_t pid;
+
+	HANDLE curr_proc { nullptr };
+
+	explicit process(const std::string_view process_name);
+};
+
+
+int main()
+{
+	const auto a = process("scattern_panner.exe");
+}
